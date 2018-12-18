0e957c842296f8e190eac40a8d002dd229f09336
gavin <gavinaa123aah@163.com>
2018-12-19 02:06:38 +0800

    0.1 框架搭建:springboot+springcloud

---
+++ restaurant_service_a/src/test/java/com/example/restaurant_service_a/RestaurantServiceAApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.restaurant_service_a;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantServiceAApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
